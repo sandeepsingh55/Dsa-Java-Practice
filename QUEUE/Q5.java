54e09924611ad2a56b3639ec94f721518f18c71c
Sandeep Singh <singhcibu@gmail.com>
2026-06-11 23:03:12 +0530

    reverse the Queue

---
+++ QUEUE/Q5.java
@@ -0,0 +1,28 @@
+package QUEUE;
+
+import java.util.*;
+
+public class Q5 {
+    public static void reverse(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= 5; i++) {
+            q.add(i);
+        }
+        reverse(q);
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + " ");
+        }
+        System.out.println();
+    }
+}
