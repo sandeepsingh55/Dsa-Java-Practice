d0dd9307d4ceba90fd5d344535366c2cdc18ad3b
Sandeep Singh <singhcibu@gmail.com>
2026-06-26 21:00:47 +0530

    Diameter of tree

--- Binarytree/treeheight.java
+++ Binarytree/treeheight.java
@@ -13,6 +13,7 @@ public class treeheight {
         }
     }
 
+    // Height of tree
     public static int height(Node root) {
         if (root == null) {
             return 0;
@@ -23,6 +24,7 @@ public class treeheight {
 
     }
 
+    // Count the node in tree
     public static int count(Node root) {
         if (root == null) {
             return 0;
@@ -33,6 +35,33 @@ public class treeheight {
         return leftcount + rightcouint + 1;
     }
 
+    // sum of NOde in tree
+    public static int sum(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftsum = sum(root.left);
+        int rightsum = sum(root.right);
+
+        return leftsum + rightsum + root.data;
+    }
+
+    // Diameter of Tree
+
+    public static int Diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int left_dia = Diameter(root.left);
+        int right_dia = Diameter(root.right);
+        int left_height = height(root.left);
+        int right_height = height(root.right);
+        int self_dia = left_height + right_height + 1;
+
+        return Math.max(self_dia, Math.max(left_dia, right_dia));
+
+    }
+
     public static void main(String[] args) {
         Node root = new Node(1);
         root.left = new Node(2);
@@ -41,8 +70,10 @@ public class treeheight {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        System.out.println(height(root));
-        System.out.println(count(root));
+        System.out.println("Height of tree" + height(root));
+        System.out.println("total count of nodes in tree " + count(root));
+        System.out.println("Sum of nodes " + sum(root));
+        System.out.println("Diameter of tree " + Diameter(root));
 
     }
 }
